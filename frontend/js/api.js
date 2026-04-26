68e221f363e66703f698dc00b861fb2b1d7c42a1
ajaykumar2783 <ajaykumaracchu@gmail.com>
2026-04-26 17:24:50 +0530

    connect frontend to live backend

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,2 +1,3 @@
 const API_BASE = "http://127.0.0.1:5000/api";
-const API_BASE_URL = "https://inventory-management-3-0ya0.onrender.com";
+// const API_BASE_URL = "https://inventory-management-3-0ya0.onrender.com";
+const API_BASE_URL = "https://inventory-management-cu2e.onrender.com";
